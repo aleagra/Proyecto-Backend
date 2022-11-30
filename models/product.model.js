544597bea987bf51b259e71053318b8d8d7d17c2
Alejandro Agra <aleagra08@hotmail.com>
2022-11-30 14:28:40 -0300

    JOI

--- models/product.model.js
+++ models/product.model.js
@@ -1,11 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Joi = require("joi");
 
 const UsersSchema = new Schema({
   titulo_producto: String,
   texto_producto: String,
   precio_producto: String,
   tipo_procuto: String,
+  status: {
+    type: Boolean,
+    default: true,
+  },
 });
 
-module.exports = mongoose.model("product", UsersSchema);
+const Product = mongoose.model("product", UsersSchema);
+
+const ValidateUser = (user) => {
+  const schema = Joi.object({
+    titulo_producto: Joi.string().min(5).max(100).required(),
+    tipo_procuto: Joi.string().min(5).max(100).required(),
+  }).options({ abortEarly: false });
+
+  return schema.validate(user);
+};
+
+module.exports = {
+  Product,
+  ValidateUser,
+};
